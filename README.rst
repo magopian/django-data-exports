63f25ea106b762b54a62cd0467c0b81ad01f59b8
Mathieu Agopian <mathieu@trustive.com>
2011-05-31 15:52:56 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,15 @@
+Django-data-exports
+===================
+
+Django-data-exports is a model data exports for Django. It allows you to easily
+create exports for your models.
+
+Adding this app to your project will let you create exports for your models,
+and customize the data that will be exported using filters, and custom
+templates.
+
+* Authors: see AUTHORS
+* Licence: BSD
+* Compatibility: Django 1.3+ (class-based-views required)
+* Requirements: none
+* Documentation: http://django-data-exports.readthedocs.org/en/latest/
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+import data_exports
+
+
+setup(
+    name='django-data-exports',
+    version=data_exports.__version__,
+    author=u'Mathieu agopian',
+    author_email='mathieu.agopian@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/magopian/django-data-exports',
+    license='BSD licence, see LICENCE file',
+    description='Model data exports for Django',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django>=1.3',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+)
