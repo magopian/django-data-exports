68a6c9354b4f73dc5d46bf0c8159e5594f05128e
Mathieu Agopian <mathieu@trustive.com>
2011-06-17 10:12:50 +0200

    slightly refactored 'getattribute' filter

--- data_exports/templatetags/getter_tags.py
+++ data_exports/templatetags/getter_tags.py
@@ -8,28 +8,28 @@ register = template.Library()
 
 @register.filter()
 def getattribute(model, item):
-    """Equivalent to getattr(model, item)
+    """Chained lookup of item on model
 
-    If item has dots (eg: foo.bar.baz), recursively call getattribute():
+    If item has dots (eg: 'foo.bar.baz'), recursively call getattribute():
     e = getattr(model, 'foo')
     e = getattr(e, 'bar')
     e = getattr(e, 'baz')
-    Then return e, or e() if it's a callable
+    At each step, check if e is a callable, and if so, use e()
 
     """
     elements = item.split('.')
     element = elements.pop(0)
     attr = getattr(model, element, None)
     if attr is None: # end of recursion
-        return attr
-    if not elements: # no elements left (end of recursion)
-        if callable(attr):
-            try:
-                return attr()
-            except: # couldn't call this method without params after all
-                return ''
-        return attr
-    return getattribute(attr, '.'.join(elements))
+        return
+    if callable(attr):
+        try:
+            attr = attr()
+        except: # couldn't call this method without params
+            return
+    if elements:
+        return getattribute(attr, '.'.join(elements))
+    return attr # no elements left (end of recursion)
 
 @register.filter()
 def getvalue(dictionary, item):
