9564363fcc88c85936a624180b70f7ebd414a0d8
Christophe Narbonne <Christophe31@gmail.com>
2015-04-23 10:28:52 +0200

    Enable django 1.7+ migrations to avoid issue #12

--- data_exports/migrations/0001_initial.py
+++ data_exports/migrations/0001_initial.py
@@ -1,65 +1,70 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Export'
-        db.create_table('data_exports_export', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-            ('display_labels', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('model', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-        ))
-        db.send_create_signal('data_exports', ['Export'])
-
-        # Adding model 'Column'
-        db.create_table('data_exports_column', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('export', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['data_exports.Export'])),
-            ('column', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('label', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-        ))
-        db.send_create_signal('data_exports', ['Column'])
+from django.db import models, migrations
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Export'
-        db.delete_table('data_exports_export')
+class Migration(migrations.Migration):
 
-        # Deleting model 'Column'
-        db.delete_table('data_exports_column')
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
 
-
-    models = {
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'data_exports.column': {
-            'Meta': {'object_name': 'Column'},
-            'column': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'export': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['data_exports.Export']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'label': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
-        },
-        'data_exports.export': {
-            'Meta': {'object_name': 'Export'},
-            'display_labels': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        }
-    }
-
-    complete_apps = ['data_exports']
+    operations = [
+        migrations.CreateModel(
+            name='Column',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('column', models.CharField(max_length=255)),
+                ('label', models.CharField(max_length=255, blank=True)),
+                ('order', models.PositiveIntegerField()),
+            ],
+            options={
+                'ordering': ['order'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Export',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('slug', models.SlugField(unique=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Format',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('file_ext', models.CharField(max_length=10, blank=True)),
+                ('mime', models.CharField(max_length=50)),
+                ('template', models.CharField(max_length=255)),
+            ],
+            options={
+                'ordering': ['name'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='export',
+            name='export_format',
+            field=models.ForeignKey(blank=True, to='data_exports.Format', help_text='Leave empty to display as HTML', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='export',
+            name='model',
+            field=models.ForeignKey(to='contenttypes.ContentType'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='column',
+            name='export',
+            field=models.ForeignKey(to='data_exports.Export'),
+            preserve_default=True,
+        ),
+    ]
