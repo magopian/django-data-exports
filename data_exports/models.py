da8aed9ac62b4fd1df3d6db4f12fd4efe0f612d1
Narbonne <Christophe31@users.noreply.github.com>
2017-12-08 06:09:34 +0000

    dj2.0 quick fix reverse import

--- data_exports/models.py
+++ data_exports/models.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
